84f8a1fd00c9e78fe564df3410b88fba330ad63a
aakashkishan@outlook.com <aakash_kishan@outlook.com>
2017-02-04 00:39:45 +0530

    Added Basic Speech Recognition Functionality

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 *	Running the Server in port number 4000.
 */
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var path = require('path');
 
 app.get('/', function (req, res) {
-  res.send('index.html');
+  res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.listen(4000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 4000!');
 })
